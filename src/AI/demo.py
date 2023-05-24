f427cfa27cf449515e85d3fed80cf7b383288f59
jawad <jawad@192.168.39.180>
2023-05-24 15:31:11 +0300

    Python added

--- src/AI/demo.py
+++ src/AI/demo.py
@@ -41,29 +41,32 @@ def generate_image(prompt, num_image=1, size='512x512', output_format='url'):
         logging.error("Unexpected error: ", exc_info=True)
         return None
 
-config = configparser.ConfigParser()
-config.read('credential.ini')
-API_KEY = config['openai']['APIKEY']
+#config = configparser.ConfigParser()
+#config.read('credential.ini')
 
-openai.api_key = API_KEY
 
+#API_KEY = config['openai']['APIKEY']
+print("jawad")
+openai.api_key = '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'
+print("makhoul")
 SIZES = ('1024x1024', '512x512', '256x256')
 
 # Get image description from user
-image_description = input("Enter the image description: ")
-
+image_description = sys.argv[1]
+print("21342134234")
 # Specify the folder where you want to save the images
-output_folder = os.path.join("C:\\Users\\jawad\\git\\Awni-wood-work\\src\\Lib", 'images')
-
+output_folder = os.path.join("C:\\Users\\jawad\\git\\Awni-wood-work\\src\\Lib\\images")
+print("2")
 # Generate images (byte output)
-response = generate_image(image_description, num_image=3, size=SIZES[1], output_format='b64_json')
+response = generate_image(image_description, num_image=10, size=SIZES[1], output_format='b64_json')
 if response is not None:
     prefix = 'demo'
     for indx, image in enumerate(response['images']):
         # Combine the output folder path with the file name
-        output_path = os.path.join(output_folder, f"{prefix}_{indx}.jpg")
+        output_path = os.path.join(output_folder, f"{prefix}_{indx+1}.jpg")
         with open(output_path, 'wb') as f:
             f.write(b64decode(image))
     logging.info(f"Images successfully saved in {output_folder}")
 else:
     logging.error("Failed to generate byte images.")
+print("3")
